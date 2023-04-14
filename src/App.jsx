9edf29e622dc9ed52be30a556932829c8170dee0
tyVespA <migliavacca.marco08@gmail.com>
2023-04-14 12:35:31 +0200

    Add state to handle countryData values

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import "./App.css";
 import Question from "../components/Question";
+import { useState } from "react";
 import { useEffect } from "react";
 import { getData } from "../helpers/getData";
 
 function App() {
-  getData();
+  const [countryData, setCountryData] = useState(null);
+
+  async function handleGetData() {
+    const data = await getData();
+    setCountryData(data);
+  }
+
+  if (!countryData) {
+    handleGetData();
+    return <div>Loading...</div>;
+  }
+
+  const { countryName, countryCapital } = countryData;
+
   return (
     <div className="App">
-      {/* <Question /> */}
+      <Question />
       {/* <div>test: {dataJson}</div> */}
+      {/* <p>
+        The capital of {countryName} is {countryCapital}
+      </p> */}
     </div>
   );
 }
