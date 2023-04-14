a345abc20d98cab7af18aa35b38a13713acfb50d
tyVespA <migliavacca.marco08@gmail.com>
2023-04-14 12:35:02 +0200

    Updata getData to return random country and capital

--- helpers/getData.js
+++ helpers/getData.js
@@ -1,6 +1,16 @@
+import { randomIntFromInterval } from "./randomIntFromInterval";
+
 export async function getData() {
   const dataString = await fetch("https://restcountries.com/v3.1/all");
-  console.log(dataString);
   const dataJson = await dataString.json();
   console.log(dataJson);
+
+  const randomCountryIndex = randomIntFromInterval(0, 250);
+
+  const countryName = dataJson[randomCountryIndex]["name"]["common"];
+  console.log(countryName);
+  const countryCapital = dataJson[randomCountryIndex]["capital"][0];
+  console.log(countryCapital);
+
+  return { countryName, countryCapital };
 }
