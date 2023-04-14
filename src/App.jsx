b97bcc943318092640f93ae5cab983e1bebff0e8
tyVespA <migliavacca.marco08@gmail.com>
2023-04-14 12:06:38 +0200

    Add getData function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Question from "../components/Question";
+import { useEffect } from "react";
 
 function App() {
+  async function getData() {
+    const dataString = await fetch("https://restcountries.com/v3.1/all");
+    console.log(dataString);
+    const dataJson = await dataString.json();
+    console.log(dataJson);
+  }
+  getData();
   return (
     <div className="App">
-      <Question />
+      {/* <Question /> */}
+      {/* <div>test: {dataJson}</div> */}
     </div>
   );
 }
